358fb8244ae6b5b391bda7827bc65abae55fb181
Omar Bakier <omar.bakeer@vezeeta.com>
2021-05-04 02:24:57 +0200

    test(list): add test cases for UI view

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('Add list component is showing in all cases', () => {
   render(<App />);
-  const linkElement = screen.getByText(/^\+ Add .*(list)$/i);
-  expect(linkElement).toBeInTheDocument();
+  const addListLabel = screen.getByText(/^\+ Add .*(list)$/i);
+  expect(addListLabel).toBeInTheDocument();
 });

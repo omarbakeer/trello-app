62cb0c2cd470c3c65e8f3059ea7b2603e212f64d
Omar Bakier <omar.bakeer@vezeeta.com>
2021-05-07 05:11:36 +0200

    feat(list): progress in adding list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import AddList from './components/List/AddList';
+import React, { useReducer } from 'react';
+import AddItem from './components/AddItem';
+import List from './components/List';
+import { reducer, initialState, actions} from './state'
 import './App.css';
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  
+  const addANewList = newList => {
+    dispatch(actions.createNewList(newList));
+  }
+
   return (
     <div className="App">
-      <AddList />
+      {state.lists.map(list => (
+        <List listLabel={list.listLabel}></List>
+      ))}
+      <AddItem addNewListToReducer={addANewList} />
     </div>
   );
 }

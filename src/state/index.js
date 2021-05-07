f24a850c37d944b0b0779938bf70d06b57ac4f68
Omar Bakier <omar.bakeer@vezeeta.com>
2021-05-07 13:15:15 +0200

    feat(state): introduce context api and a custom hook

--- src/state/index.js
+++ src/state/index.js
@@ -1,6 +1,7 @@
 import initialState from './initialState';
 import actionTypes from './actionTypes';
 import * as actions from './actions';
+import { useCustomContext } from './context';
 import reducer from './reducer';
 
-export { initialState, actionTypes, actions, reducer };
+export { initialState, actionTypes, actions, reducer, useCustomContext };
